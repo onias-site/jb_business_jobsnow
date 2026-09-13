4d5dcf9ba36fbea79cf74fe7d1eed7952d6c7d9c
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2026-09-12 23:57:39 -0300

    reformando sistema de mensagens

--- src/main/java/com/jb/entities/JbEntityBotCommandStepStartMessage.java
+++ src/main/java/com/jb/entities/JbEntityBotCommandStepStartMessage.java
@@ -13,7 +13,6 @@ import com.ccp.especifications.db.utils.entity.decorators.annotations.CcpEntityF
 import com.ccp.especifications.db.utils.entity.decorators.annotations.CcpEntityFieldsValidator;
 import com.ccp.especifications.db.utils.entity.decorators.annotations.CcpEntityVersionable;
 import com.ccp.especifications.db.utils.entity.decorators.engine.CcpEntityFactory;
-import com.ccp.especifications.db.utils.entity.decorators.engine.CcpEntityMetaData;
 import com.ccp.especifications.db.utils.entity.decorators.interfaces.CcpEntityConfigurator;
 import com.ccp.especifications.db.utils.entity.fields.annotations.CcpEntityFieldPrimaryKey;
 import com.ccp.especifications.http.CcpHttpContentType;
@@ -21,14 +20,10 @@ import com.ccp.json.validations.fields.annotations.CcpJsonCopyFieldValidationsFr
 import com.ccp.json.validations.fields.annotations.CcpJsonFieldValidatorRequired;
 import com.jb.business.bots.engine.JbSupportBotCommands;
 import com.jn.business.messages.JnInstantMessageType;
-import com.jn.entities.JnEntityLoginTokenRequestResend;
-import com.jn.entities.JnEntityLoginTokenRequestUnlock;
-import com.jn.entities.JnEntitySystemMessage;
 import com.jn.entities.decorators.JnVersionableEntity;
 import com.jn.entities.fields.transformers.JnJsonTransformersFieldsEntityDefault;
 import com.jn.json.fields.validation.JnJsonCommonsFields;
 import com.jn.json.fields.validation.JnJsonInstantMessengerFields;
-
 import com.jn.utils.JnLanguage;
 
 @CcpEntityCache(3600)
@@ -70,23 +65,6 @@ public class JbEntityBotCommandStepStartMessage implements CcpEntityConfigurator
 		;
 	}
 	
-	private CcpBulkItem toSystemMessage(CcpEntity entity, JnLanguage language, String message) {
-		String languageName = language.name();
-		CcpJsonRepresentation put = CcpOtherConstants.EMPTY_JSON
-		.put(JnJsonCommonsFields.language, languageName);
-		String entityName = entity.name();
-		CcpJsonRepresentation put2 = put
-		.put(JnEntitySystemMessage.Fields.systemMessageName, entityName);
-
-		CcpJsonRepresentation json = put2
-		.put(JnJsonCommonsFields.message, message)
-		;
-
-		CcpEntityMetaData entityMetaData = JnEntitySystemMessage.ENTITY.getEntityMetaData();
-		CcpBulkItem createBulkItem = entityMetaData.toCreateBulkItem(json);
-		return createBulkItem;
-	}
-	
 	public List<CcpBulkItem> getFirstRecordsToInsert() {
 		String valorMais = "O usuário '{" + JnJsonCommonsFields.email;
 	
@@ -106,17 +84,10 @@ public class JbEntityBotCommandStepStartMessage implements CcpEntityConfigurator
 		.put(JnJsonInstantMessengerFields.caption, caption)
 		;
 	  
-		CcpBulkItem resend = this.toSystemMessage(JnEntityLoginTokenRequestResend.ENTITY, JnLanguage.portuguese, "não recebeu e-mail com token");
-		
-		CcpBulkItem unlock = this.toSystemMessage(JnEntityLoginTokenRequestUnlock.ENTITY, JnLanguage.portuguese, "teve o token bloqueado");
 		List<CcpBulkItem> toCreateBulkItems = CcpEntityConfigurator.super.toCreateBulkItems(ENTITY, endMessage);
 
 		List<CcpBulkItem> createBulkItems = new ArrayList<CcpBulkItem>(toCreateBulkItems);
 		 
-		createBulkItems.add(resend);
-		 
-		createBulkItems.add(unlock);
-
 		return createBulkItems;
 	}
 }
